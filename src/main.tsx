34573a0e405058e5729950e714b44f567ed14b8b
LindsayCoral <lahainalindsay9111@gmail.com>
2026-09-14 18:05:18 -1000

    Remove obsolete Vite browser entry

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import TypewellApp from "./App";
-import "./styles.css";
-
-createRoot(document.getElementById("root")!).render(<TypewellApp initialPath={window.location.pathname} />);
