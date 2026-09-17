7412be5bf4f4a8ea2e1b58b53725167c30ef1923
LindsayCoral <lahainalindsay9111@gmail.com>
2026-09-17 12:28:12 -1000

    Replace legacy Typewell naming in src/ClientApp.tsx

--- src/ClientApp.tsx
+++ src/ClientApp.tsx
@@ -2,9 +2,9 @@
 
 import dynamic from "next/dynamic";
 
-const TypewellApp = dynamic(() => import("./App"), {
+const WPMTestApp = dynamic(() => import("./App"), {
   ssr: false,
   loading: () => <div className="app-loading" style={{ minHeight: "100vh" }} aria-hidden="true" />
 });
 
-export default TypewellApp;
+export default WPMTestApp;
